d96a307872f63ff8cc8f38e26669e5b0a6003a95
Nata Kalina <natakalinatester@gmail.com>
2023-12-06 22:47:17 -0500

    Lesson 1.8 Data Fetching, Re-Fetching, and Memoized Handlers V.1

--- src/App.js
+++ src/App.js
@@ -2,60 +2,66 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-const initialTodos = [
-  {
-    id: '1',
-    title: 'Complete lesson 1.2 React',
-    url: 'https://learn.codethedream.org/flamingo-react/',
-  },
-  {
-    id: '2',
-    title: 'Complete lesson 1.2 Ruby',
-    url: 'https://learn.codethedream.org/firefish-ruby-rails/',
-  },
-  {
-    id: '3',
-    title: 'Complete lesson 1.1 DSA',
-    url: 'https://github.com/a-nyx/dsa-maple-code',
-  },
-];
-
 function App() {
-  //const [todoList, setTodoList] = useSemiPersistentState();
 
   const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem('savedTodoList')) || initialTodos
+  //  JSON.parse(localStorage.getItem('savedTodoList')) 
   );
 
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  const getTodoList = () =>
-    new Promise((resolve, reject) =>
-      setTimeout(
-        () =>
-          resolve({
-            data: { todoList: initialTodos },
-          }),
-        2000
-      )
-    );
-
-  React.useEffect(() => {
-    getTodoList().then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
-  }, []);
+const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     const savedTodoList = JSON.stringify(todoList);
-    if (isLoading === false) {
+    if (isLoading == false) {
       localStorage.setItem('savedTodoList', savedTodoList);
     }
-  }, [todoList, isLoading]);
+  }, [todoList]);
+  
+  const fetchData = async () => {
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+      },
+    };
 
-  // return [todoList, setTodoList];
+    try {
+      const response = await fetch(url, options);
 
+      if (!response.ok) {
+        const message = `Error: ${response.status}`;
+        throw new Error(message);
+      }
+
+      const data = await response.json();
+      //console.log(data);
+
+      const todos = data.records.map((todo) => {
+        const newTodo = {
+          id: todo.id,
+          title: todo.fields.Title,
+        };
+
+       return newTodo;
+       
+      });
+      console.log(todos);
+      setTodoList(todos);
+
+      
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchData().then((result) => {
+      setTodoList(result);
+      setIsLoading(false);
+    });
+  }, []);
+  
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
